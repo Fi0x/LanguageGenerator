31117f7ea9db182572abf35d1a74cfa344b6a284
Fi0x <felpech@aol.com>
2023-01-28 10:24:04 +0100

    Added check for forbidden combinations to randomizer

--- src/main/java/io/fi0x/Randomizer.java
+++ src/main/java/io/fi0x/Randomizer.java
@@ -1,5 +1,6 @@
 package io.fi0x;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class Randomizer
@@ -8,39 +9,46 @@ public class Randomizer
     {
         StringBuilder name = new StringBuilder();
         int desiredLength = (int) (Math.random() * (LanguageTraits.maxNameLength - LanguageTraits.minNameLength) + LanguageTraits.minNameLength);
-        for(int i = (int) (Math.random() * 4); name.length() < desiredLength; i++)
+        for(int i = (int) (Math.random() * 4); name.length() < desiredLength && i < desiredLength + 4; i++)
         {
+            ArrayList<String> selectedList;
             switch(i % 4)
             {
-                case 0 -> name.append(vokalEnd(name.toString()));
-                case 1 -> name.append(randomKonsonant(name.toString()));
-                case 2 -> name.append(vokalStart(name.toString()));
-                default -> name.append(randomVokal(name.toString()));
+                case 0 -> selectedList = LanguageTraits.consonantVocals;
+                case 1 -> selectedList = LanguageTraits.consonants;
+                case 2 -> selectedList = LanguageTraits.vocalConsonant;
+                default -> selectedList = LanguageTraits.vocals;
             }
+            name.append(getNewRandom(name.toString(), selectedList));
         }
 
         name.setCharAt(0, String.valueOf(name.charAt(0)).toUpperCase(Locale.ROOT).charAt(0));
         return name.toString();
     }
 
-    private static String vokalEnd(String previousLetters)
+    private static String getNewRandom(String previousLetters, ArrayList<String> newPossibilities)
     {
-        //TODO: Add forbidden combination check
-        return LanguageTraits.consonantVocals.get((int) (Math.random() * LanguageTraits.consonantVocals.size()));
+        int tries = 0;
+        int randomIdx = (int) (Math.random() * newPossibilities.size());
+
+        while(tries < newPossibilities.size())
+        {
+            String nextPossiblePart = newPossibilities.get((randomIdx + tries) % newPossibilities.size());
+
+            if(isAllowed(previousLetters, nextPossiblePart))
+                return nextPossiblePart;
+
+            tries++;
+        }
+        return "";
     }
-    private static String vokalStart(String previousLetters)
+    private static boolean isAllowed(String originalPart, String newPart)
     {
-        //TODO: Add forbidden combination check
-        return LanguageTraits.vocalConsonant.get((int) (Math.random() * LanguageTraits.vocalConsonant.size()));
-    }
-    private static String randomVokal(String previousLetters)
-    {
-        //TODO: Add forbidden combination check
-        return LanguageTraits.vocals.get((int) (Math.random() * LanguageTraits.vocals.size()));
-    }
-    private static String randomKonsonant(String previousLetters)
-    {
-        //TODO: Add forbidden combination check
-        return LanguageTraits.consonants.get((int) (Math.random() * LanguageTraits.consonants.size()));
+        for(String forbidden : LanguageTraits.forbiddenCombinations)
+        {
+            if((originalPart + newPart).contains(forbidden))
+                return false;
+        }
+        return true;
     }
 }
