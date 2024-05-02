da8c2273d56c2f970b42b78cde1e92583fe3c601
Felix Pechtl <felix.pechtl@ametras.com>
2024-05-02 14:54:59 +0200

    Using request param and generator to generate words

--- src/main/java/io/fi0x/MainApplication.java
+++ src/main/java/io/fi0x/MainApplication.java
@@ -1,11 +1,23 @@
 package io.fi0x;
 
+import io.fi0x.logic.FileLoader;
+import io.fi0x.logic.LanguageTraits;
+import io.fi0x.logic.Setup;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MainApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception
+    {
+        Setup.createDefaultFileStructure();
+        Setup.createTemplateLanguageFile();
+        FileLoader.loadAllLanguageFiles();
+        LanguageTraits.loadTemplateLanguage();
+        log.info("Initial loading complete");
+
         SpringApplication.run(MainApplication.class, args);
     }
 }
--- src/main/java/io/fi0x/rest/LanguageController.java
+++ src/main/java/io/fi0x/rest/LanguageController.java
@@ -1,25 +1,34 @@
 package io.fi0x.rest;
 
+import io.fi0x.logic.LanguageTraits;
+import io.fi0x.logic.Randomizer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class LanguageController
 {
-    @GetMapping("/generate-word/{language}")
-    public String generateWord(ModelMap model, @PathVariable(value = "language") String language)
+    @GetMapping("/generate-words/{language}")
+    public String generateWord(ModelMap model, @PathVariable(value = "language") String language,
+                               @RequestParam(value = "amount", defaultValue = "1", required = false) int amount)
     {
+        LanguageTraits.loadTemplateLanguage();
         // TODO: Load correct language-file
-        //  Use Generator to generate a single word
 
-        List<Word> wordList = new ArrayList<>();
-        wordList.add(new Word(language, "Test Word"));
+        Randomizer.generateWords(amount);
+        List<Word> wordList = Randomizer.getGeneratedWords().stream().map((word) -> convert(language, word)).toList();
         model.put("words", wordList);
+
         return "wordView";
+    }
+
+    private Word convert(String language, String word)
+    {
+        return new Word(language, word);
     }
 }
