1da8ea8058e3328b027a3eb4e09e921330600925
Fi0x <felpech@aol.com>
2024-05-15 19:06:34 +0200

    Updated spring security config to use db for users

--- src/main/java/io/fi0x/languagegenerator/config/SpringSecurityConfig.java
+++ src/main/java/io/fi0x/languagegenerator/config/SpringSecurityConfig.java
@@ -2,21 +2,58 @@ package io.fi0x.languagegenerator.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringSecurityConfig
 {
     @Bean
-    public InMemoryUserDetailsManager createUserDetails()
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
-        return new InMemoryUserDetailsManager(createUser("fi0x", "123"),
-                createUser("dummy1", "456"),
-                createUser("dummy2", "789"));
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        return http.build();
+    }
+
+    @Bean
+    public DataSource dataSource()
+    {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource)
+    {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+
+        createUser(manager, "fi0x", "123", new String[]{"USER", "ADMIN"});
+        createUser(manager, "dummy1", "456", new String[]{"USER"});
+
+        return manager;
     }
 
     @Bean
@@ -25,8 +62,8 @@ public class SpringSecurityConfig
         return new BCryptPasswordEncoder();
     }
 
-    private UserDetails createUser(String username, String password)
+    private void createUser(JdbcUserDetailsManager userManager, String username, String password, String[] roles)
     {
-        return User.builder().passwordEncoder(input -> passwordEncoder().encode(input)).username(username).password(password).roles("USER", "ADMIN").build();
+        userManager.createUser(User.builder().passwordEncoder(input -> passwordEncoder().encode(input)).username(username).password(password).roles(roles).build());
     }
 }
