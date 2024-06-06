3079107de1881b95a6947f5c5a11776088fe8b76
Fi0x <felpech@aol.com>
2024-06-06 15:22:18 +0200

    Added working unit-test for AuthenticationService

--- src/test/java/io/fi0x/languagegenerator/service/TestAuthenticationService.java
+++ src/test/java/io/fi0x/languagegenerator/service/TestAuthenticationService.java
@@ -1,13 +1,84 @@
 package io.fi0x.languagegenerator.service;
 
+import io.fi0x.languagegenerator.logic.dto.UserDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
 public class TestAuthenticationService
 {
+    private static final String USERNAME = "Authenticated Username";
+
+    @Mock
+    private UserDetailsManager userDetailsManager;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private Authentication authentication;
+    @Mock
+    private SecurityContext securityContext;
+
+    @InjectMocks
+    private AuthenticationService service;
+
+    @BeforeEach
+    void setup()
+    {
+        MockitoAnnotations.openMocks(this);
+        doNothing().when(userDetailsManager).createUser(any());
+    }
+
     @Test
     @Tag("UnitTest")
     void test_getAuthenticatedUsername_success()
     {
+        mockStatic(SecurityContextHolder.class).when(SecurityContextHolder::getContext).thenReturn(securityContext);
+        doReturn(authentication).when(securityContext).getAuthentication();
+        doReturn(USERNAME).when(authentication).getName();
+
+        Assertions.assertEquals(USERNAME, service.getAuthenticatedUsername());
+    }
+
+    @Test
+    @Tag("UnitTest")
+    void test_registerUser_success()
+    {
+        when(userDetailsManager.userExists(any())).thenReturn(false);
+        when(passwordEncoder.encode(any())).thenReturn("!@#QWE234retfvg");
+
+        Assertions.assertDoesNotThrow(() -> service.registerUser(getUserDto()));
+    }
+
+    @Test
+    @Tag("UnitTest")
+    void test_registerUser_error()
+    {
+        when(userDetailsManager.userExists(any())).thenReturn(true);
+
+        Assertions.assertThrows(DuplicateKeyException.class, () -> service.registerUser(getUserDto()));
+    }
+
+    private UserDto getUserDto()
+    {
+        UserDto dto = new UserDto();
+        dto.setUsername("AAA a d ddd ds");
+        dto.setPassword("AAAA");
+        dto.setMatchingPassword("AAAA");
+        return dto;
     }
 }
