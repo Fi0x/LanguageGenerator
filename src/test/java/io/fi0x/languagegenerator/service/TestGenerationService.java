67a71db00240e7e252946042cecf9aab01183068
Fi0x <felpech@aol.com>
2024-06-29 13:44:17 +0200

    Completed important tests

--- src/test/java/io/fi0x/languagegenerator/service/TestGenerationService.java
+++ src/test/java/io/fi0x/languagegenerator/service/TestGenerationService.java
@@ -30,15 +30,13 @@ public class TestGenerationService
     private static final String NAME = "Language Name";
     private static final String USERNAME = "Karl Heinz";
     private static final boolean VISIBLE = false;
-    private static final int MIN_WORD_LENGTH = 2;
-    private static final int MAX_WORD_LENGTH = 2;
+    private static final int NORMAL_WORD_LENGTH = 2;
+    private static final int BEGINNING_END_WORD_LENGTH = 4;
     private static final Double SPECIAL_CHARACTER_CHANCE = 1D;
     private static final String DEFAULT_LETTER = "a";
     private static final Long DEFAULT_LETTER_ID = 4L;
     private static final String BEGINNING_LETTER = "b";
     private static final Long BEGINNING_LETTER_ID = 8L;
-    private static final String SPECIAL_LETTER = "'";
-    private static final Long SPECIAL_LETTER_ID = 16L;
     private static final String END_LETTER = "c";
     private static final Long END_LETTER_ID = 32L;
 
@@ -71,7 +69,7 @@ public class TestGenerationService
     {
         MockitoAnnotations.openMocks(this);
 
-        doReturn(getLanguage()).when(languageRepository).findById(eq(VALID_LANGUAGE_ID));
+        doReturn(getLanguage(NORMAL_WORD_LENGTH)).when(languageRepository).findById(eq(VALID_LANGUAGE_ID));
     }
 
     @Test
@@ -117,16 +115,20 @@ public class TestGenerationService
 
     @Test
     @Tag("UnitTest")
-    void test_generateWords_BeginningAndEnd()
+    void test_generateWords_BeginningAndEnd() throws InvalidObjectException
     {
-        //TODO: Complete these tests
-    }
+        doReturn(getConsonantCombinations()).when(cRepository).getAllByLanguageId(eq(VALID_LANGUAGE_ID));
+        doReturn(getVocalCombinations()).when(vRepository).getAllByLanguageId(eq(VALID_LANGUAGE_ID));
+        doReturn(getConsonantVocalCombinations()).when(cvRepository).getAllByLanguageId(eq(VALID_LANGUAGE_ID));
+        doReturn(getVocalConsonantCombinations()).when(vcRepository).getAllByLanguageId(eq(VALID_LANGUAGE_ID));
+        doReturn(getBeginningCombinations()).when(staRepository).getAllByLanguageId(eq(VALID_LANGUAGE_ID));
+        doReturn(getEndCombinations()).when(endRepository).getAllByLanguageId(eq(VALID_LANGUAGE_ID));
+        doReturn(getDefaultLetter()).when(letterRepository).findById(eq(DEFAULT_LETTER_ID));
+        doReturn(getBeginningLetter()).when(letterRepository).findById(eq(BEGINNING_LETTER_ID));
+        doReturn(getEndLetter()).when(letterRepository).findById(eq(END_LETTER_ID));
+        doReturn(getLanguage(BEGINNING_END_WORD_LENGTH)).when(languageRepository).findById(eq(VALID_LANGUAGE_ID));
 
-    @Test
-    @Tag("UnitTest")
-    void test_generateWords_SpecialCharacters()
-    {
-        //TODO: Complete these tests
+        Assertions.assertEquals(getBeginningEndWordList(), service.generateWords(VALID_LANGUAGE_ID, 4));
     }
 
     private List<Word> getNormalWordList()
@@ -149,16 +151,6 @@ public class TestGenerationService
         return words;
     }
 
-    private List<Word> getSpecialWordList()
-    {
-        List<Word> words = new ArrayList<>();
-        words.add(new Word(VALID_LANGUAGE_ID, "A'a"));
-        words.add(new Word(VALID_LANGUAGE_ID, "A'a"));
-        words.add(new Word(VALID_LANGUAGE_ID, "A'a"));
-        words.add(new Word(VALID_LANGUAGE_ID, "A'a"));
-        return words;
-    }
-
     private List<Word> getEmptyWordList()
     {
         List<Word> words = new ArrayList<>();
@@ -169,15 +161,15 @@ public class TestGenerationService
         return words;
     }
 
-    private Optional<Language> getLanguage()
+    private Optional<Language> getLanguage(int wordLength)
     {
         Language language = new Language();
         language.setId(VALID_LANGUAGE_ID);
         language.setName(NAME);
         language.setUsername(USERNAME);
         language.setVisible(VISIBLE);
-        language.setMaxWordLength(MIN_WORD_LENGTH);
-        language.setMaxWordLength(MAX_WORD_LENGTH);
+        language.setMinWordLength(wordLength);
+        language.setMaxWordLength(wordLength);
         language.setSpecialCharacterChance(SPECIAL_CHARACTER_CHANCE);
         return Optional.of(language);
     }
@@ -227,10 +219,42 @@ public class TestGenerationService
         return result;
     }
 
+    private List<StartingCombinations> getBeginningCombinations()
+    {
+        List<StartingCombinations> result = new ArrayList<>();
+        StartingCombinations combination = new StartingCombinations();
+        combination.setLetterId(BEGINNING_LETTER_ID);
+        result.add(combination);
+        return result;
+    }
+
+    private List<EndingCombinations> getEndCombinations()
+    {
+        List<EndingCombinations> result = new ArrayList<>();
+        EndingCombinations combination = new EndingCombinations();
+        combination.setLetterId(END_LETTER_ID);
+        result.add(combination);
+        return result;
+    }
+
     private Optional<Letter> getDefaultLetter()
     {
         Letter letter = new Letter();
         letter.setLetters(DEFAULT_LETTER);
+        return Optional.of(letter);
+    }
+
+    private Optional<Letter> getBeginningLetter()
+    {
+        Letter letter = new Letter();
+        letter.setLetters(BEGINNING_LETTER);
+        return Optional.of(letter);
+    }
+
+    private Optional<Letter> getEndLetter()
+    {
+        Letter letter = new Letter();
+        letter.setLetters(END_LETTER);
         return Optional.of(letter);
     }
 }
