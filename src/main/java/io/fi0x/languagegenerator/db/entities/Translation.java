dad50a0a69fadcbe22890d68b6d9f001cafc2547
Fi0x <felpech@aol.com>
2024-07-15 15:44:03 +0200

    Added new tables
    - Save words of a language
    - Link words with ones of other languages

---
+++ src/main/java/io/fi0x/languagegenerator/db/entities/Translation.java
@@ -0,0 +1,35 @@
+package io.fi0x.languagegenerator.db.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@IdClass(Translation.TranslationId.class)
+@Table(name = "TRANS")
+public class Translation
+{
+    @Id
+    private Long languageId;
+    @Id
+    private Long wordNumber;
+    @Id
+    private Long translatedLanguageId;
+    @Id
+    private Long translatedWordNumber;
+
+    @AllArgsConstructor
+    public class TranslationId implements Serializable
+    {
+        private Long languageId;
+        private Long wordNumber;
+        private Long translatedLanguageId;
+        private Long translatedWordNumber;
+    }
+}
