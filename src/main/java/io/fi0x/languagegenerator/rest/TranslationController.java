37359a426f5a76d1ab7189516a666375c9d49e7d
Fi0x <felpech@aol.com>
2024-07-19 16:23:03 +0200

    Added new endpoint to save words

---
+++ src/main/java/io/fi0x/languagegenerator/rest/TranslationController.java
@@ -0,0 +1,31 @@
+package io.fi0x.languagegenerator.rest;
+
+import io.fi0x.languagegenerator.logic.dto.WordDto;
+import io.fi0x.languagegenerator.service.TranslationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Slf4j
+@Controller
+@AllArgsConstructor
+public class TranslationController
+{
+    private TranslationService translationService;
+
+    @Transactional
+    @PostMapping("word")
+    public String saveWord(ModelMap model, HttpServletRequest request, @Valid WordDto word)
+    {
+        log.info("saveWord() called for word={}", word);
+
+        translationService.saveOrGetWord(word);
+
+        return "redirect:" + request.getHeader("Referer");
+    }
+}
